6bb86ca157570210ed84ac11c069aae76dc42044
Andrea Cattaneo <andrea@cattaneo.dev>
2020-01-05 12:01:20 +0100

    Show depencencies on mouse enter

--- src/FirstSetsLookAheadTable.svelte
+++ src/FirstSetsLookAheadTable.svelte
@@ -17,15 +17,8 @@
     }
   }
 
-  function onCellClick(l, index, iteration) {
-    const newSelection = `${l}_${index}_${iteration}`;
-    if (selectedCell === newSelection) {
-      selectedCell = undefined;
-      dependencyCells = [];
-      return;
-    }
-
-    selectedCell = newSelection;
+  function onCellEnter(l, index, iteration) {
+    selectedCell = `${l}_${index}_${iteration}`;
     dependencyCells = [];
 
     if (iteration >= 1) {
@@ -34,11 +27,16 @@
       });
     }
   }
+
+  function onCellLeave() {
+    selectedCell = undefined;
+    dependencyCells = [];
+  }
 </script>
 
 <style>
   .separator {
-    background-color: rgb(255, 240, 165);
+    background-color: #efefef;
   }
 </style>
 
@@ -60,7 +58,8 @@
         </td>
         {#each firstSets[l][index] as set, iteration}
           <td
-            on:click={onCellClick(l, index, iteration)}
+            on:mouseenter={onCellEnter(l, index, iteration)}
+            on:mouseleave={onCellLeave}
             class:selected={selectedCell === `${l}_${index}_${iteration}`}
             class:dependency={dependencyCells.includes(`${l}_${iteration}`)}>
             {#if set.length}{`{${set}}`}{:else}∅{/if}
