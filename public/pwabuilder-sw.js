962e9ff6ea3c29f95df1e5ba3abdee8e0a22a1be
Andrea Cattaneo <andrea@cattaneo.dev>
2020-01-19 15:16:49 +0100

    Remove console.log()

--- public/pwabuilder-sw.js
+++ public/pwabuilder-sw.js
@@ -7,12 +7,6 @@ const offlineFallbackPage = "index.html";
 self.addEventListener("install", function (event) {
     event.waitUntil(
         caches.open(CACHE).then(function (cache) {
-            console.log("[PWA Builder] Cached offline page during install");
-
-            if (offlineFallbackPage === "ToDo-replace-this-name.html") {
-                return cache.add(new Response("TODO: Update the value of the offlineFallbackPage constant in the serviceworker."));
-            }
-
             return cache.add(offlineFallbackPage);
         })
     );
